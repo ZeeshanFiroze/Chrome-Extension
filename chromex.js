374f6acd09fa489836df01bc5c37e14014bbbbf4
ZeeshanFiroze <zeeshanfirozeabdulla@gmail.com>
2026-01-21 23:16:53 +0530

    Small changes

--- chromex.js
+++ chromex.js
@@ -1,12 +1,14 @@
-let myleads = [];
+let myleads = ["111","222","333"];
 const inputbtn=document.getElementById("input-btn");
 const inputel=document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el"); 
 inputbtn.addEventListener("click", function()
 {
     myleads.push(inputel.value); 
-}); 
-for (let i=0; i<myleads.length; i++)
+})
+let listitems = "";
+for (let i=0; i<myleads.length+1; i++) 
 {
-    ulEl.innerHTML += "<li>" + myleads[i] + "</li>";
-}
+    listitems += "<li>" + myleads[i] + "</li>";
+ }
+ulEl.innerHTML = listitems;
